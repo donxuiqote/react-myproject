91506ed4688ba9f29aac15e23f797aa64fabdfe9
donxuiqote <endinrhmnd@gmail.com>
2023-05-31 15:12:14 +0700

    Membuat Navbar

---
+++ my-project/src/components/Fontawesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, faEnvelope, faHome, faUser, faPhone, faBars, faTimes);
---
+++ my-project/src/components/MenuItems.js
@@ -0,0 +1,20 @@
+export const MenuItems = [
+    {
+        title : "Home",
+        url : "/",
+        cName : "nav-links",
+        icon : "fa-solid fa-home"
+    },
+    {
+        title : "Portofolio",
+        url : "/portofolio",
+        cName : "nav-links",
+        icon : "fa-solid fa-user"
+    },
+    {
+        title : "Contact",
+        url : "/contact",
+        cName : "nav-links",
+        icon : "fa-solid fa-phone"
+    }
+]
